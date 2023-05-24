e8eca93ed43f6de067712cb69378d2aeb94648dd
SiyoonKIM <neosinee@pukyong.ac.kr>
2023-05-24 19:46:49 +0900

    [Feature]#19: QuizPage 생성

--- App.js
+++ App.js
@@ -4,6 +4,8 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import LandingPage from "../client/src/pages/LandingPage"
 import LoginPage from "../client/src/pages/LoginPage"
+import QuizPage from "../client/src/pages/QuizPage"
+import QuizPage1 from "../client/src/pages/QuizPage1"
 
 const Stack = createNativeStackNavigator();
 
@@ -11,6 +13,8 @@ function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator>
+        <Stack.Screen name="Quiz" component={QuizPage} />
+        <Stack.Screen name="Quiz1" component={QuizPage1} />
         <Stack.Screen name="Landing" component={LandingPage} />
         <Stack.Screen name="Login" component={LoginPage} />
       </Stack.Navigator>
