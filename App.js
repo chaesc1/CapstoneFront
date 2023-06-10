bfa40ef66382da7e4a35e39da49547198a229a36
chaeyeon1 <slide10245@naver.com>
2023-06-10 20:15:16 +0900

    #25 반응형 작업하기

--- App.js
+++ App.js
@@ -1,8 +1,8 @@
-import * as React from "react";
-
-import { View, Text } from "react-native";
+import React, { useState, useEffect } from "react";
+import { View, Text, StyleSheet, Dimensions } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { RecoilRoot, useRecoilState } from "recoil";
 
 import LandingPage from "../client/src/pages/LandingPage";
 import LoginPage from "../client/src/pages/LoginPage";
@@ -11,66 +11,70 @@ import RegisterPage from "../client/src/pages/RegisterPage";
 import MemberMainPage from "../client/src/pages/MemberMainPage";
 import GuestMainPage from "../client/src/pages/GuestMainPage";
 import CounselingPage from "./src/pages/CounselingPage";
-import WordChainPage from "./src/pages/WordChainPage";
+import WordChainPage from "./src/pages/wordChain/WordChainPage";
 import TwentyQuestionPage from "./src/pages/TwentyQuestionPage";
 import WordChainLandingPage from "./src/pages/WordChainLandingPage";
 
 const Stack = createNativeStackNavigator();
 
-
 function App() {
   return (
-    <NavigationContainer>
-      <Stack.Navigator>
+    <RecoilRoot>
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen
+            name="WordChainLanding"
+            component={WordChainLandingPage}
+          />
+          <Stack.Screen
+            name="LandingPage"
+            component={LandingPage}
+            options={{
+              title: "",
+              headerBackTitleVisible: false,
+              headerBackImage: "BackBtn",
+              headerShown: false,
+            }}
+          />
 
-      <Stack.Screen
-          name="LandingPage"
-          component={LandingPage}
-          options={{
-            title: "",
-            headerBackTitleVisible: false,
-            headerBackImage: "BackBtn",
-            headerShown: false,
-          }}
-        />
-        
-        <Stack.Screen name="Guest" component={GuestMainPage} />
-        <Stack.Screen  name="WordChainLanding"  component={WordChainLandingPage} />
-        <Stack.Screen name="TwentyQuestion" component={TwentyQuestionPage} />
-        <Stack.Screen name="Counseling" component={CounselingPage} />
+          <Stack.Screen name="Guest" component={GuestMainPage} />
 
-        <Stack.Screen name="WordChain" component={WordChainPage} />
-        <Stack.Screen name="Member" component={MemberMainPage} />
-        {/* <Stack.Screen name="Landing" component={LandingPage} /> */}
+          <Stack.Screen name="TwentyQuestion" component={TwentyQuestionPage} />
+          <Stack.Screen name="Counseling" component={CounselingPage} />
 
-        {/* LoginPage */}
-        <Stack.Screen
-          name="Login"
-          component={LoginPage}
-          options={{
-            title: "",
-            headerBackTitleVisible: false,
-            headerBackImage: "BackBtn",
-            headerStyle: {
-              backgroundColor: "#f3e99f",
-            },
-          }}
-        />
+          <Stack.Screen name="WordChain" component={WordChainPage} />
+          <Stack.Screen name="Member" component={MemberMainPage} />
+          {/* <Stack.Screen name="Landing" component={LandingPage} /> */}
 
-        <Stack.Screen
-          name="Register"
-          component={RegisterPage}
-          options={{
-            title: "",
-            headerBackTitleVisible: false,
-            headerBackImage: "BackBtn",
-            headerStyle: {
-              backgroundColor: "#f3e99f",
-            },
-          }}
-        />
-      </Stack.Navigator>
-    </NavigationContainer>
+          {/* LoginPage */}
+          <Stack.Screen
+            name="Login"
+            component={LoginPage}
+            options={{
+              title: "",
+              headerBackTitleVisible: false,
+              headerBackImage: "BackBtn",
+              headerStyle: {
+                backgroundColor: "#f3e99f",
+              },
+            }}
+          />
+
+          <Stack.Screen
+            name="Register"
+            component={RegisterPage}
+            options={{
+              title: "",
+              headerBackTitleVisible: false,
+              headerBackImage: "BackBtn",
+              headerStyle: {
+                backgroundColor: "#f3e99f",
+              },
+            }}
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </RecoilRoot>
   );
 }
 
